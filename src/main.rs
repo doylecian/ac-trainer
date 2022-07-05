a573db93ec707d9c1099f3efdc166cb17d14536a
doylecian <ciandoyle64@gmail.com>
2022-07-05 02:38:24 +0100

    code refactor

--- src/main.rs
+++ src/main.rs
@@ -25,26 +25,20 @@ fn main() {
 
 	let (game_handle, proc_addr) = process_init("ac_client.exe").expect("Cannot initialise game hack - make sure the game is running");
 	let mut client = Game::new(proc_addr, game_handle);
-	let frametime = time::Duration::from_millis(10);
-
-	// println!("North East: {:.4}", angle_to_y((3.0, 2.0)));
-	// println!("North West {:.4}", angle_to_y((-3.0, 2.0)));
-	// println!("South West {:.4}", angle_to_y((-3.0, -2.0)));
-	// println!("South East {:.4}", angle_to_y((3.0, -2.0)));
 
 	if let Some(v) = resolve_pointer_chain(client.proc_handle, client.base_address, &Y_VIEW_OFFSETS, AddressType::Pointer) {
 		client.add_address("viewTable".to_string(), v);
 	}
 
-	// if let Some(ent_list_addr) = resolve_pointer_chain(client.proc_handle, client.base_address, &ENTITY_LIST_START, AddressType::Pointer) {
-	// 	let mut ent_list = EntityList::new(ent_list_addr, 0x4, 5, client.proc_handle);
-	// 	ent_list.populate(5);
-	// 	client.add_entity_list("Bot player list", ent_list);
-	// }
+	if let Some(ent_list_addr) = resolve_pointer_chain(client.proc_handle, client.base_address, &ENTITY_LIST_START, AddressType::Pointer) {
+		let mut ent_list = EntityList::new(ent_list_addr, 0x4, 5, client.proc_handle);
+		ent_list.populate(5);
+		client.add_entity_list("Bot player list", ent_list);
+	}
 
 	loop {
 		update(&mut client);
-		thread::sleep(frametime);
+		thread::sleep(time::Duration::from_millis(10));
 		unsafe {
 			if GetAsyncKeyState(0x02) != 0 {
 				match write_float(client.proc_handle, client.value_pointers["viewTable"] + 0x248, 123.0, 4) {
@@ -58,10 +52,6 @@ fn main() {
 				}
 			}
 			if GetAsyncKeyState(0x10) != 0 {
-				match write_float(client.proc_handle, client.value_pointers["viewTable"] + 0x248, 123.0, 4) {
-					Ok(_) => println!("Wrote to view angle address"),
-					Err(msg) => println!("Error message: {:?}", msg)
-				}
 				CloseHandle(client.proc_handle);
 				return;
 			}
@@ -78,19 +68,19 @@ fn update(client: &mut Game) {
 	let y_view = read_mem_addr(client.proc_handle, client.value_pointers["viewTable"] + 0x24C, 4).unwrap();
 
 	println!("LocalPlayer -> x: {:.4}, y: {:.4} // xView: {:.4} yView: {:.4}", f32::from_bits(local_player_xpos as u32), f32::from_bits(local_player_ypos as u32), f32::from_bits(x_view as u32), f32::from_bits(y_view as u32));
-	//println!("Player count: {}", read_mem_addr(client.proc_handle, client.base_address + PLAYER_COUNT_OFFSET, 4).unwrap());
-	// for (_, list) in &mut client.entity_lists {
-	// 	for bot in &list.ent_vec {
-	// 		let dist = euclid_dist((f32::from_bits(local_player_xpos as u32), f32::from_bits(local_player_ypos as u32)), (bot.position.x, bot.position.y));
-	// 		if dist < 60.0 {
-	// 			println!("BOT {:?} -> {{x: {:.4}, y: {:.4}}} \x1b[93m({}m)\x1b[0m", bot.name, bot.position.x, bot.position.y, dist as u32 / 10);
-	// 		}
-	// 		else {
-	// 			println!("BOT {:?} -> {{x: {:.4}, y: {:.4}}} ({}m)", bot.name, bot.position.x, bot.position.y, dist as u32 / 10);
-	// 		}
-	// 	}
-	// 	list.refresh();
-	// }
+	println!("Player count: {}", read_mem_addr(client.proc_handle, client.base_address + PLAYER_COUNT_OFFSET, 4).unwrap());
+	for (_, list) in &mut client.entity_lists {
+		for bot in &list.ent_vec {
+			let dist = euclid_dist((f32::from_bits(local_player_xpos as u32), f32::from_bits(local_player_ypos as u32)), (bot.position.x, bot.position.y));
+			if dist < 60.0 {
+				println!("BOT {:?} -> {{x: {:.4}, y: {:.4}}} \x1b[93m({}m)\x1b[0m", bot.name, bot.position.x, bot.position.y, dist as u32 / 10);
+			}
+			else {
+				println!("BOT {:?} -> {{x: {:.4}, y: {:.4}}} ({}m)", bot.name, bot.position.x, bot.position.y, dist as u32 / 10);
+			}
+		}
+		list.refresh();
+	}
 	print!("{}[2J", 27 as char);
 }
 
@@ -110,18 +100,5 @@ fn process_init(proc_name: &str) -> Result<(usize, HANDLE), String> {
 	println!("[rust_client] Found game process and created handle with PROCESS_VM_READ");
 	Ok((proc_addr, proc_handle))
 }
-
-// for bot in &ent_list.ent_vec {
-// 	let dist = euclid_dist((f32::from_bits(local_player_xpos as u32), f32::from_bits(local_player_ypos as u32)), (bot.position.x, bot.position.y));
-// 	if dist < 60.0 {
-// 		println!("BOT {:?} -> {{x: {:.4}, y: {:.4}}} \x1b[93m({}m)\x1b[0m", bot.name, bot.position.x, bot.position.y, dist as u32 / 10);
-
-// 	}
-// 	else {
-// 		println!("BOT {:?} -> {{x: {:.4}, y: {:.4}}} ({}m)", bot.name, bot.position.x, bot.position.y, dist as u32 / 10);
-// 	}
-// }
-//ent_list.refresh();
-
 
 	
